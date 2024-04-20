ebbb471025ba2cd89f44c54a04b4c0661362f544
norba <norbaa79@gmail.com>
2024-04-20 09:59:44 +0200

    Initial commit

---
+++ src/main/java/hu/webler/footievault/FootieVaultApplication.java
@@ -0,0 +1,13 @@
+package hu.webler.footievault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootieVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootieVaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/webler/footievault/FootieVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webler.footievault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootieVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
